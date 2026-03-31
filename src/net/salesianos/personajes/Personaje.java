331d12d340cb2ba94f4e5830b3ca753027fb7195
Nick <usr4227@salesianos-lacuesta.net>
2026-03-31 21:05:04 +0100

    Métodos de Personaje

--- src/net/salesianos/personajes/Personaje.java
+++ src/net/salesianos/personajes/Personaje.java
@@ -38,4 +38,43 @@ public class Personaje {
         this.energia = energia;
     }
 
+    public void entrenar(int x) {
+        if (x <= 0) {
+            System.out.println("No se puede entrenar 0 o menos niveles.");
+            return;
+        }
+        nivel = nivel + x;
+    }
+
+    public void descansar(int x) {
+        if (x <= 0) {
+            System.out.println("No se puede descansar 0 o menos puntos.");
+            return;
+        }
+        energia = energia + x;
+        if (energia > 100) {
+            energia = 100;
+        }
+    }
+
+    public void gastarEnergia(int x) {
+        if (x <= 0) {
+            System.out.println("No se puede gastar 0 o menos puntos.");
+            return;
+        }
+        if (energia < 0) {
+            energia = 0;
+        }
+    }
+
+    public void mostrarInfo() {
+        System.out.println("\nNombre: " + nombre);
+        System.out.println("Nivel: " + nivel);
+        System.out.println("Energía: " + energia);
+    }
+
+    public static void mostrarPersonajes() {
+        System.out.println("\nTotal personajes creados: " + totalPersonajes);
+    }
+
 }
